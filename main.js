311c0b06e13fb421abce22ab3f6125177d4c3c56
arjun380 <82092827+arjun380@users.noreply.github.com>
2021-09-13 15:52:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-https://teachablemachine.withgoogle.com/models/4l31CuQpp/
+//https://teachablemachine.withgoogle.com/models/4l31CuQpp/
+Webcam.set(
+    {
+        width: 350,
+        height:300,
+        image_format:'png',
+        png_quality:90
+    });
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function snapshort()
+{ Webcam.snap(function(data_uri){
+         document.getElementById("result").innerHTML='<img id="captured_img" src="'+data_uri+'">';
+          });
+}console.log("ml5 version",ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/4l31CuQpp/model.json',modelLoaded);
+function modelLoaded()
+{
+    console.log("model is loaded");
+}
